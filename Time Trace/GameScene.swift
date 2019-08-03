7f6eafe10e0db23d31e0b09f2a31cbc6c515456a
Ashley Granitto <aeg249@cornell.edu>
2019-08-03 18:18:50 -0400

    testing push/pull

--- Time Trace/GameScene.swift
+++ Time Trace/GameScene.swift
@@ -16,6 +16,7 @@ class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
         // This is a test, hi ashley
+        // This is another test, hi connor
         // Get label node from scene and store it for use later
         self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
         if let label = self.label {
