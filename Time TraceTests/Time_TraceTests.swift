0a6d8cd178d09d52e94143a1cc24c6a95fd3afc6
Ashley & Connor <ca339@cornell.edu>
2019-08-02 22:56:38 -0400

    Setting up repo

---
+++ Time TraceTests/Time_TraceTests.swift
@@ -0,0 +1,34 @@
+//
+//  Time_TraceTests.swift
+//  Time TraceTests
+//
+//  Created by Connor Anderson on 8/2/19.
+//  Copyright © 2019 TTI. All rights reserved.
+//
+
+import XCTest
+@testable import Time_Trace
+
+class Time_TraceTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
