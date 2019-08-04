e5bad145663287f3d9216416ab706ec496162026
Ashley Granitto <aeg249@cornell.edu>
2019-08-03 21:53:38 -0400

    Circular touch detection, elimination of irrelevant code

--- Time Trace/GameScene.swift
+++ Time Trace/GameScene.swift
@@ -2,7 +2,7 @@
 //  GameScene.swift
 //  Time Trace
 //
-//  Created by Connor Anderson on 8/2/19.
+//  Created by Connor Anderson & Ashley Granitto on 8/2/19.
 //  Copyright © 2019 TTI. All rights reserved.
 //
 
@@ -11,63 +11,52 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    // Variable declaration
+    private var touchDetection : SKShapeNode?
     
+    // Creates a fading circle shape node to track the user's touch movements.
     override func didMove(to view: SKView) {
-        // This is a test, hi ashley
-        // This is another test, hi connor
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+        self.touchDetection = SKShapeNode.init(circleOfRadius: (self.size.width + self.size.height) * 0.01)
+        if let circleIndicator = self.touchDetection {
+            circleIndicator.run(SKAction.sequence([SKAction.fadeOut(withDuration: 0.25),                                                         SKAction.removeFromParent()]))
         }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        let pathLine = SKShapeNode.init(rectOf: CGSize(width: 50, height: 50))
+        pathLine.position = CGPoint(x: self.size.width/2, y: self.size.height/2)
+        pathLine.zPosition = 1
+        pathLine.fillColor = SKColor.blue
+        self.addChild(pathLine)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
     }
     
-    
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+        if let n = self.touchDetection?.copy() as! SKShapeNode? {
             n.position = pos
             n.strokeColor = SKColor.green
+            n.fillColor = SKColor.green
             self.addChild(n)
         }
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+        if let n = self.touchDetection?.copy() as! SKShapeNode? {
             n.position = pos
             n.strokeColor = SKColor.blue
+            n.fillColor = SKColor.blue
             self.addChild(n)
         }
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+        if let n = self.touchDetection?.copy() as! SKShapeNode? {
             n.position = pos
             n.strokeColor = SKColor.red
+            n.fillColor = SKColor.red
             self.addChild(n)
         }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
         for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
@@ -86,5 +75,9 @@ class GameScene: SKScene {
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+    }
+    
+    func drawPath(){
+        
     }
 }
