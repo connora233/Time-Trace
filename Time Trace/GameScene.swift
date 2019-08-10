1296f6e68dc41fe6a60ec3169232c93a2a7ad7bc
Ashley Granitto <aeg249@cornell.edu>
2019-08-09 23:33:33 -0400

    Implements nextCoordinateEnd(), nextCoordinateStart(), addPath() to create functioning path generation; edits to other functions and elimination of unnecessary code

--- Time Trace/GameScene.swift
+++ Time Trace/GameScene.swift
@@ -22,14 +22,18 @@ class GameScene: SKScene {
     private var colorArray : Array<UIColor> = [UIColor.red, UIColor.orange, UIColor.yellow, UIColor.green, UIColor.blue, UIColor.purple]
     
     // Variable declaration for the rectangle-in-use's data
-    private var currentEndPoint : CGPoint = CGPoint(x: 0, y: 0)
+    private var currentCenterEnd : CGPoint = CGPoint(x: 0, y: 0)
+    private var currentCenterStart : CGPoint = CGPoint(x: 0, y: 0)
     private var currentStartPoint : CGPoint = CGPoint(x: 0, y: 0)
     private var currentPosition : CGPoint = CGPoint(x: 0, y: 0)
-    private var currentAngle : Int = 45
-    private var currentHeight : Int = 200
+    private var currentAngle : Int = 0
+    private var currentHeight : Int = 300
     private var currentWidth : CGFloat = 60
     private var currentRadius : CGFloat = 30
-    private var test : Int = 0
+    
+    // Variable declaration for pathway alignment
+    private var end : CGPoint = CGPoint(x: 0, y: 0)
+    private var start : CGPoint = CGPoint(x: 0, y: 0)
     
     // Creates a fading circle shape node to track the user's touch movements. Draws a rectangles in a random location on the screen.
     override func didMove(to view: SKView) {
@@ -41,11 +45,141 @@ class GameScene: SKScene {
         pathInitializer()
     }
     
-    //Generates the original three rectangles in the first pathway.
+    // Generates the original three rectangles in the first pathway.
     func pathInitializer(){
+        currentAngle = Int.random(in: 0...360)
+        drawRect(newPoint: currentStartPoint, height: Int.random(in: 200...400), angle: currentAngle)
+        for _ in 1...5{
+            addPath()
+        }
+    }
+    
+    // Generates additional paths based upon data from prior paths.
+    func addPath() {
+        let endPoint = nextCoordinateEnd()
+        currentAngle = Int.random(in: 0...360)
+        drawRect(newPoint: nextCoordinateStart(currentAngle: currentAngle), height: Int.random(in: 200...400), angle: currentAngle)
+    }
+    
+    // Returns a coordinate that can be used by future generated rectangles to align their position to the proper location on the screen.
+    func nextCoordinateEnd() -> CGPoint {
+        let vectorLength = (pow(currentWidth/2, 2) + pow(CGFloat(currentHeight)-currentRadius, 2)).squareRoot()
+        let vectorAngle = atan((CGFloat(currentHeight)-currentRadius) / (currentWidth/2))
+        let angleAddition = (CGFloat(currentAngle) * CGFloat.pi/180)
+        let adjustedAngle = vectorAngle + angleAddition
+        
+        let newX = vectorLength * cos(adjustedAngle)
+        let newY = vectorLength * sin(adjustedAngle)
+        
+        end = CGPoint(x: newX + currentStartPoint.x, y: newY + currentStartPoint.y)
+        
+        return end
+    }
+    
+    // Returns a coordinate that (when used as the position of a newly generated rectangle) lines up the new pathway with the second most-recent pathway.
+    func nextCoordinateStart(currentAngle: Int) -> CGPoint {
+        let vectorLength = (pow(currentWidth/2, 2) + pow(CGFloat(currentRadius), 2)).squareRoot()
+        let vectorAngle = atan(CGFloat(currentRadius) / (currentWidth/2))
+        let angleAddition = (CGFloat(currentAngle) * CGFloat.pi/180)
+        let adjustedAngle = vectorAngle + angleAddition
+        
+        let newX = vectorLength * cos(adjustedAngle)
+        let newY = vectorLength * sin(adjustedAngle)
+        
+        start = CGPoint(x: end.x - newX, y: end.y - newY)
+       
+        return start
+    }
+    
+    //Draws a rectangular path using SKShapeObject().
+    func drawRect(newPoint: CGPoint, height: Int, angle: Int){
+        currentStartPoint = newPoint
+        currentHeight = height
+        currentAngle = angle
+        
+        let shape = SKShapeNode()
+        shape.position = CGPoint(x: currentStartPoint.x, y: currentStartPoint.y)
+        shape.path = UIBezierPath(roundedRect: CGRect(x: 0, y: 0, width: currentWidth, height: CGFloat(currentHeight)), cornerRadius: currentRadius).cgPath
+        
+        let color = changeColor()
+        shape.fillColor = color
+        shape.strokeColor = color
+        shape.alpha = 0.4
+        addChild(shape)
+        shapeArray.append(shape)
+        
+        let radians = CGFloat.pi * CGFloat(angle) / 180
+        shape.zRotation = CGFloat(radians)
+    }
+    
+    // Returns the next color in colorArray.
+    func changeColor() -> UIColor {
+        let modulus = colorTracker % 6
+        colorTracker += 1
+        return colorArray[modulus]
+    }
+    
+    func touchDown(atPoint pos : CGPoint) {
+        if let n = self.touchDetection?.copy() as! SKShapeNode? {
+            n.position = pos
+            n.strokeColor = SKColor.green
+            n.fillColor = SKColor.green
+            self.addChild(n)
+            
+            print(pos)
+        }
+    }
+    
+    func touchMoved(toPoint pos : CGPoint) {
+        if let n = self.touchDetection?.copy() as! SKShapeNode? {
+            n.position = pos
+            n.strokeColor = SKColor.blue
+            n.fillColor = SKColor.blue
+            self.addChild(n)
+        }
+    }
+    
+    func touchUp(atPoint pos : CGPoint) {
+        if let n = self.touchDetection?.copy() as! SKShapeNode? {
+            n.position = pos
+            n.strokeColor = SKColor.red
+            n.fillColor = SKColor.red
+            self.addChild(n)
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first!
+        for shape in shapeArray{
+            if shape.contains(touch.location(in: self)) {
+                print("touched")
+            }
+        }
+        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first!
+        for shape in shapeArray{
+            if shape.contains(touch.location(in: self)) {
+                print("touched")
+            }
+        }
+        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    // Temporarily moved--workability to be determined in a future edit.
+    /*func inLine() {
         drawRect(newPoint: currentStartPoint, height: currentHeight, angle: Int.random(in: 0...360))
         for _ in 1...5{
-            let next = nextCoordinate(oldPoint: currentStartPoint)
             let xB = CGFloat(1)
             let yB = CGFloat(350)
             let low = 200
@@ -115,133 +249,5 @@ class GameScene: SKScene {
                 drawRect(newPoint: next, height: Int.random(in: low...high), angle: Int.random(in: 40...50))
             }
         }
-    }
-    
-    //Returns the appropriate start coordinate based upon the previous line's data.
-    func nextCoordinate(oldPoint: CGPoint) -> CGPoint{
-        return CGPoint(x: currentStartPoint.x - CGFloat(currentHeight) * sin(CGFloat((CGFloat(currentAngle) * CGFloat.pi/180))), y: currentStartPoint.y + CGFloat(currentHeight) * cos((CGFloat(currentAngle) * CGFloat.pi/180)))
-    }
-    
-    //Draws a rectangular path using SKShapeObject().
-    func drawRect(newPoint: CGPoint, height: Int, angle: Int){
-        currentEndPoint = currentStartPoint
-        print("currentEnd")
-        print(currentEndPoint)
-        currentStartPoint = newPoint
-        print("currentStart")
-        print(currentStartPoint)
-        currentHeight = height
-        currentAngle = angle
-        let color = changeColor()
-        let shape = SKShapeNode()
-        //Right here we decide where the path will eventually be moved by calling to adjustment()---needs a lot of work I just tried a few ideas but none were very functional
-        shape.position = adjustment(end: currentEndPoint, start: currentStartPoint)
-        print("position")
-        print(shape.position)
-        //Draws the path at 0,0, but the position we already established will "translate it" to the desired position
-        shape.path = UIBezierPath(roundedRect: CGRect(x: 0, y: 0, width: currentWidth, height: CGFloat(currentHeight)), cornerRadius: currentRadius).cgPath
-        shape.fillColor = color
-        shape.strokeColor = color
-        shape.alpha = 0.4
-        addChild(shape)
-        shapeArray.append(shape)
-        
-        //I tried line 83 as a way of bettering adjustment()--might be more harm than good, not vital to drawRect()
-        currentStartPoint = shape.position
-        
-        //NOTE!!!! All angles are in radians everywhere, but this function takes in degrees. If you try to rotate elsewhere, make sure you're converting
-        let radians = CGFloat.pi * CGFloat(angle) / 180
-        shape.zRotation = CGFloat(radians)
-    }
-    
-    //NEEDS OPTIMIZING!!! Attempts to move the rotated path to the correct position.
-    func adjustment(end: CGPoint, start: CGPoint) -> CGPoint{
-        var xCord = CGFloat(0.0)
-        var yCord = CGFloat(0.0)
-        if currentEndPoint.x < currentStartPoint.x {
-            xCord = currentStartPoint.x + currentWidth/2
-        }
-        else if currentEndPoint.x == currentStartPoint.x {
-            xCord = currentStartPoint.x
-        }
-        else{
-            xCord = currentStartPoint.x - currentWidth/2
-        }
-        
-        if currentEndPoint.y < currentStartPoint.y {
-            yCord = currentStartPoint.y - currentWidth/2
-        }
-        else if currentEndPoint.y == currentStartPoint.y {
-            yCord = currentStartPoint.y
-        }
-        else{
-            yCord = currentStartPoint.y + currentWidth/2
-        }
-        return CGPoint(x: xCord, y: yCord)
-
-    }
-    
-    // Returns the next color in colorArray.
-    func changeColor() -> UIColor {
-        let modulus = colorTracker % 6
-        colorTracker += 1
-        return colorArray[modulus]
-    }
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.touchDetection?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            n.fillColor = SKColor.green
-            self.addChild(n)
-            
-            print(pos)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.touchDetection?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            n.fillColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.touchDetection?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            n.fillColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        let touch = touches.first!
-        for shape in shapeArray{
-            if shape.contains(touch.location(in: self)) {
-                print("touched")
-            }
-        }
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        let touch = touches.first!
-        for shape in shapeArray{
-            if shape.contains(touch.location(in: self)) {
-                print("touched")
-            }
-        }
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
+    }*/
 }
