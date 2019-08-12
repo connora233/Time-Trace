10fbc35782a46d34ff3d1a106a9bb818541e04d6
Connor <ca339@cornell.edu>
2019-08-11 21:03:17 -0400

    Start screen animation

--- Time Trace/StartScreen.swift
+++ Time Trace/StartScreen.swift
@@ -11,26 +11,33 @@ import GameplayKit
 
 class StartScreen: SKScene {
     
+    let gameScene = GameScene(fileNamed: "GameScene")
+
     // Variable declaration for general constants
     private var touchDetection : SKShapeNode?
     private var shapeArray : Array<SKShapeNode> = []
-    private var screenWidth : Int = Int(UIScreen.main.bounds.width)
-    private var screenHeight : Int = Int(UIScreen.main.bounds.height)
-    let gameScene = GameScene(fileNamed: "GameScene")
+    private var ghostArray : Array<SKShapeNode> = []
+    private var screenWidth : CGFloat = CGFloat(UIScreen.main.bounds.width)
+    private var screenHeight : CGFloat = CGFloat(UIScreen.main.bounds.height)
     
     // Variable declaration for path color changing
     private var colorTracker : Int = 0
     private var colorArray : Array<UIColor> = [UIColor.red, UIColor.orange, UIColor.yellow, UIColor.green, UIColor.blue, UIColor.purple]
     
     // Variable declaration for the rectangle-in-use's data
-    private var currentEndPoint : CGPoint = CGPoint(x: 0, y: 0)
+    private var currentCenterEnd : CGPoint = CGPoint(x: 0, y: 0)
+    private var currentCenterStart : CGPoint = CGPoint(x: 0, y: 0)
     private var currentStartPoint : CGPoint = CGPoint(x: 0, y: 0)
     private var currentPosition : CGPoint = CGPoint(x: 0, y: 0)
-    private var currentAngle : Int = 45
-    private var currentHeight : Int = 200
+    private var currentAngle : Int = 0
+    private var currentHeight : Int = 300
     private var currentWidth : CGFloat = 60
     private var currentRadius : CGFloat = 30
-    private var test : Int = 0
+    
+    // Variable declaration for pathway alignment
+    private var end : CGPoint = CGPoint(x: 0, y: 0)
+    private var previousEnd: CGPoint = CGPoint(x: 0, y: 0)
+    private var start : CGPoint = CGPoint(x: 0, y: 0)
     
     
     override func didMove(to view: SKView) {
@@ -44,6 +51,127 @@ class StartScreen: SKScene {
         startButton.fillColor = UIColor.green
         startButton.strokeColor = UIColor.green
         addChild(startButton)
+        pathInitializer()
+    }
+    
+    //Draws initial rectangle and prepares to begin game
+    func pathInitializer(){
+        currentAngle = Int.random(in: 0...360)
+        currentHeight = Int.random(in: 200...300)
+        drawRect(newPoint: currentStartPoint, height: currentHeight, angle: currentAngle)
+        
+    }
+    
+    // Generates additional paths based upon data from prior paths and ensures they remain on the screen
+    func addPath() {
+        previousEnd = end
+        end = nextCoordinateEnd(angle: currentAngle, height: currentHeight)
+        currentAngle = Int.random(in: 0...360)
+        currentHeight = Int.random(in: 300...500)
+        var screen = false
+        while(!screen){
+            drawGhostRect(newPoint: nextCoordinateStart(currentAngle: currentAngle), height: currentHeight, angle: currentAngle)
+            if(onScreen(point: nextCoordinateEnd(angle: currentAngle, height: currentHeight))){
+                screen = true
+                ghostArray[0].removeFromParent()
+                ghostArray.remove(at: 0)
+                break
+            }
+            ghostArray[0].removeFromParent()
+            ghostArray.remove(at: 0)
+            currentAngle = Int.random(in: 0...360)
+            currentHeight = Int.random(in: 200...400)
+        }
+        drawRect(newPoint: nextCoordinateStart(currentAngle: currentAngle), height: currentHeight, angle: currentAngle)
+    }
+    
+    // Determines if a point is on the screen
+    func onScreen(point: CGPoint) -> Bool {
+        let bounds = currentRadius/2 + 10
+        if (point.x < (-300 + bounds)) || (point.x > (300 - bounds))  {
+            return false
+        }
+        if (point.y < (-630 + bounds)) || (point.y > (630 - bounds)) {
+            return false
+        }
+        if(abs(previousEnd.x - point.x) < 75 || abs(previousEnd.y - point.y) < 150) {
+            return false
+        }
+        return true
+    }
+    
+    // Returns a coordinate that can be used by future generated rectangles to align their position to the proper location on the screen.
+    func nextCoordinateEnd(angle: Int, height: Int) -> CGPoint {
+        let vectorLength = (pow(currentWidth/2, 2) + pow(CGFloat(height)-currentRadius, 2)).squareRoot()
+        let vectorAngle = atan((CGFloat(height)-currentRadius) / (currentWidth/2))
+        let angleAddition = (CGFloat(angle) * CGFloat.pi/180)
+        let adjustedAngle = vectorAngle + angleAddition
+        
+        let newX = vectorLength * cos(adjustedAngle)
+        let newY = vectorLength * sin(adjustedAngle)
+        
+        let end1 = CGPoint(x: newX + currentStartPoint.x, y: newY + currentStartPoint.y)
+        return end1
+    }
+    
+    // Returns a coordinate that (when used as the position of a newly generated rectangle) lines up the new pathway with the second most-recent pathway.
+    func nextCoordinateStart(currentAngle: Int) -> CGPoint {
+        let vectorLength = (pow(currentWidth/2, 2) + pow(CGFloat(currentRadius), 2)).squareRoot()
+        let vectorAngle = atan(CGFloat(currentRadius) / (currentWidth/2))
+        let angleAddition = (CGFloat(currentAngle) * CGFloat.pi/180)
+        let adjustedAngle = vectorAngle + angleAddition
+        
+        let newX = vectorLength * cos(adjustedAngle)
+        let newY = vectorLength * sin(adjustedAngle)
+        
+        start = CGPoint(x: end.x - newX, y: end.y - newY)
+        
+        return start
+    }
+    
+    //Draws a rectangular path using SKShapeObject().
+    func drawRect(newPoint: CGPoint, height: Int, angle: Int){
+        currentStartPoint = newPoint
+        currentHeight = height
+        currentAngle = angle
+        
+        let shape = SKShapeNode()
+        shape.position = CGPoint(x: currentStartPoint.x, y: currentStartPoint.y)
+        shape.path = UIBezierPath(roundedRect: CGRect(x: 0, y: 0, width: currentWidth, height: CGFloat(currentHeight)), cornerRadius: currentRadius).cgPath
+        
+        let color = changeColor()
+        shape.fillColor = color
+        shape.strokeColor = color
+        shape.alpha = 0.4
+        addChild(shape)
+        shapeArray.append(shape)
+        
+        let radians = CGFloat.pi * CGFloat(angle) / 180
+        shape.zRotation = CGFloat(radians)
+    }
+    
+    func drawGhostRect(newPoint: CGPoint, height: Int, angle: Int){
+        currentStartPoint = newPoint
+        currentHeight = height
+        currentAngle = angle
+        
+        let shape = SKShapeNode()
+        shape.position = CGPoint(x: currentStartPoint.x, y: currentStartPoint.y)
+        shape.path = UIBezierPath(roundedRect: CGRect(x: 0, y: 0, width: currentWidth, height: CGFloat(currentHeight)), cornerRadius: currentRadius).cgPath
+        shape.fillColor = UIColor.clear
+        shape.strokeColor = UIColor.clear
+        shape.alpha = 0.4
+        addChild(shape)
+        ghostArray.append(shape)
+        let radians = CGFloat.pi * CGFloat(angle) / 180
+        shape.zRotation = CGFloat(radians)
+    }
+    
+    // Returns the next color in colorArray.
+    func changeColor() -> UIColor {
+        let modulus = colorTracker % 6
+        colorTracker += 1
+        return colorArray[modulus]
     }
     
     func touchDown(atPoint pos : CGPoint) {
@@ -103,7 +231,15 @@ class StartScreen: SKScene {
         for t in touches { self.touchUp(atPoint: t.location(in: self)) }
     }
     
-    override func update(_ currentTime: TimeInterval) {
+    func drawLines() {
         
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+            addPath()
+            if(shapeArray.count > 3) {
+                shapeArray[0].removeFromParent()
+                shapeArray.remove(at: 0)
+            }
     }
 }
