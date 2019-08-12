0f683fb53ac79fe9a837d7e2016615a65cdeec8d
Ashley Granitto <aeg249@cornell.edu>
2019-08-11 21:57:23 -0400

    deletion of unnecessary code

--- Time Trace/GameScene.swift
+++ Time Trace/GameScene.swift
@@ -244,4 +244,6 @@ class GameScene: SKScene {
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
         for t in touches { self.touchUp(atPoint: t.location(in: self)) }
     }
+    
+    
 }
